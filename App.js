57dcf07f27db1e40f39e270d3becf3a5f0163d72
Roy Jossfolk <rjossfolk@gmail.com>
2017-12-25 15:01:25 -0500

    - copy over common components needed for UI

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/common/Header';
+import Card from './src/components/common/Card';
+import CardSection from './src/components/common/CardSection';
+import Button from './src/components/common/CardSection';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={{flex: 1}}>
-      <Header />
+        <Header />
+          <CardSection>
+            <Card />
+          </CardSection>
         
       </View>
     );

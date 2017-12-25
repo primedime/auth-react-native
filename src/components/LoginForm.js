69b79bba0d826eaf4600b8ed645252e906236370
Roy Jossfolk <rjossfolk@gmail.com>
2017-12-25 17:23:11 -0500

    - add LoginForm.js component

---
+++ src/components/LoginForm.js
@@ -0,0 +1,13 @@
+import 'React' from 'react';
+import { View } from 'react-native';
+import { Component } from 'React';
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <View />
+        );
+    }
+}
+
+export default LoginForm;

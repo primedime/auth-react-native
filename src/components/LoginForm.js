650936d5ca325f37bd1d584e807c0496d6caaa7e
Roy Jossfolk <rjossfolk@gmail.com>
2017-12-26 23:00:42 -0500

    - add sign in authentication logic w/ error handling

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { Component } from 'React';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = { email: '', password: '' };
+    state = { email: '', password: '', error: '' };
+
+    onButtonPress() {
+        const { email, password } = this.state;
+
+        this.setState({ error: '' });
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch( () => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch( () => {
+                        this.setState({ error: 'Authentication Failed.' });
+                    });
+            });
+    }
 
     render() {
         return (
@@ -26,13 +42,25 @@ class LoginForm extends Component {
                     />
                 </CardSection>
 
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Login
                     </Button>
                 </CardSection>
             </Card>
         );
+    }
+}
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
     }
 }
 
